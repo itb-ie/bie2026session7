bacbf7455b1e326a2ac973ed4c9336af7bc03876
Bogdan Ratiu <bogdan.ratiu@velocitaconsultancy.uk>
2026-02-03 17:15:36 +0100

    string examples

---
+++ basic_string.py
@@ -0,0 +1,19 @@
+s1 = "Hello World"
+s2 = 'Hello World'
+print(s1)
+print(s2)
+s1 = "FY^%&%@#$:"  # you can put weird characters
+print(s1)
+
+# we can index a string
+s = "banana"
+print(s[1]) # indexing start from zero
+print(s[0]) # first letter
+print(s[10//2])
+
+# you can also index by negatives!
+print(s[-1])
+print(s[-6]) # same as s[0]
+
+# len function will tell you the amount of characters
+print(s, len(s)) 
